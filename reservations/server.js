7725a03caf579150213f02f7c721e08b0a0badc2
Philip Manijak <github@morningshift.org>
2018-08-21 19:33:35 -0700

    reservations: Add zapier hook

--- reservations/server.js
+++ reservations/server.js
@@ -1,7 +1,14 @@
 const { json } = require('micro');
+const r2 = require('r2');
 
 module.exports = async (req, res) => {
 	const js = await json(req);
 	console.log(js);
+
+	// TODO: Send this out after replying to the client
+	let zapierHook = 'https://hooks.zapier.com/hooks/catch/3680855/qy16ij/';
+	let zapierResponse = await r2.post(zapierHook, { json: js }).json;
+	console.log(zapierResponse);
+
 	return 'Ok';
-}
+};
