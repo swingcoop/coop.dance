ef70ed40e284e5f011a38bfd47685f10ddb422ac
Philip Manijak <github@morningshift.org>
2018-11-26 11:12:59 -0800

    'now' v2
    Update everything to use 'now' v2.0.
    
    r2 wasn't working so we're using axios now.

--- api/reservations/index.js
+++ api/reservations/index.js
@@ -1,14 +1,14 @@
 const { json } = require('micro');
-const r2 = require('r2');
+const axios = require('axios');
 
 module.exports = async (req, res) => {
 	const js = await json(req);
 	console.log(js);
 
-	// TODO: Send this out after replying to the client
 	let zapierHook = 'https://hooks.zapier.com/hooks/catch/3680855/qy16ij/';
-	let zapierResponse = await r2.post(zapierHook, { json: js }).json;
-	console.log(zapierResponse);
+   let data = await axios.post(zapierHook, js);
+   console.log(data);
 
-	return 'Ok';
+   res.end("Ok");
+	return "Ok"; // TODO: Not sure if we need this 
 };
