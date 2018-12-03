f40e9902a167c289554db6b6b9d7a74332718202
Philip Manijak <github@morningshift.org>
2018-12-02 17:48:06 -0800

    Add reservations/starter-kit

---
+++ api/reservations/starter-kit/index.js
@@ -0,0 +1,42 @@
+var Airtable = require('airtable');
+// var api = require('../../subscriptions/api.js');
+
+const reservations = 'app9CS49PJx8xtYP4';
+const base = Airtable.base(reservations);
+
+function get() {
+    return new Promise(function (resolve, reject) {
+        var val = [];
+        base('Starter Kit')
+        .select({
+            fields: [ "Role", "Any" ],
+            view: "Grid View"
+        })
+        .eachPage(function page(records, nextPage) {
+            // This function (`page`) will get called for each page of records.
+
+            records.forEach(function(record) {
+              val.push(record.fields);
+            });
+
+            // To fetch the next page of records, call `fetchNextPage`.
+            // If there are more records, `page` will get called again.
+            // If there are no more records, `done` will get called.
+            nextPage();
+
+        }, function done(err) {
+            if (err) { 
+              reject(err); 
+              return; 
+            }
+            resolve(val);
+        });
+    });
+};
+
+module.exports = async (req, res) => {
+    var val = await get();
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(val));
+    return;
+};
