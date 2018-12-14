bce4f7c0f246b10ebc0e9272b44e1483113ead7d
Philip Manijak <github@morningshift.org>
2018-12-14 01:23:30 -0800

    notifications: Get all phone numbers

--- api/notifications/phones.js
+++ api/notifications/phones.js
@@ -1,37 +1,9 @@
-const axios = require('axios');
-
-var usersUrl = 'https://swing.auth0.com/api/v2/users';
-var tokenUrl = 'https://swing.auth0.com/oauth/token';
-
-var tokenRequest = {
-   grant_type: 'client_credentials',
-   client_id: process.env.AUTH0_USERS_ID,
-   client_secret: process.env.AUTH0_USERS_KEY,
-   audience: 'https://swing.auth0.com/api/v2/'
-};
+const accounts = require('./accounts.js');
 
 module.exports = () => {
-   return axios.post(tokenUrl, tokenRequest)
-   .then(res => {
-      var token = res.data.access_token;
-      var httpConfig = {
-         headers: {'Authorization': "bearer " + token}
-      };
-
-      return axios.get(usersUrl, httpConfig)
-      .then(res => {
-         var accounts = res.data;
-         if (!accounts)
-            return [];
-
-         var phoneNumbers = accounts
-            .filter(a => a.phone_number && a.phone_verified)
-            .map(a => a.phone_number);
-
-         return phoneNumbers;
-      });
-   })
-   .catch(err => {
-      throw err;
+   return accounts().then(all => {
+      return all
+         .filter(a => a.phone_number && a.phone_verified)
+         .map(a => a.phone_number);
    });
 };
