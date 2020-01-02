65a48aadb65b5a35a40b95d3c972a4cbbd7d6c6a
Philip Manijak <github@morningshift.org>
2020-01-01 23:43:02 -0800

    rsvp: Turn off when course is full

--- src/views/Rsvp.vue
+++ src/views/Rsvp.vue
@@ -6,72 +6,81 @@
 
             <p>Course: {{course && course.Title}}</p>
 
-            <p>
-                Please enter some information to reserve your spot in the class 
-                and help us understand where you're coming from.
-            </p>
-            <label>
-                <div>Name</div>
-                <input type="text" v-model="name"/>
-            </label>
+            <div v-if="courseFull">
+                <p>Thanks for your interest in this class. This course is full.</p>
 
-            <!-- <div class="agreement">
-                <div style="margin-bottom: 0.5ex">Please confirm your availability:</div>
-                <input 
-                    id="agreed" 
-                    type="checkbox"
-                    v-model="agreed"/>
-                <label for="agreed">I will be at the first session (April 3) and
-                the second session (April 10).</label>
-            </div> -->
-
-            <div>What dance role would you like to have during this class?</div>
-
-            <p>Swing dancing is a conversation, typically between two people. The person who is responsible for moving first is referred to as a lead, whereas the person who moves second is referred to as a follow. Most people who dance long enough learn both roles.</p>
-            
-            <div>Follow spots remaining: {{followSpots}}</div>
-            <div style="margin-bottom: 1em">Lead spots remaining: {{leadSpots}}</div>
-
-            <p v-if="waitlistOpen">
-                If you'd like to be added to the waitlist for this course,
-                please <a href="/contact">contact us</a>.
-            </p>
-
-            <select v-model="role">
-                <option value="null">-- Please select --</option>
-                <option v-if="followSpots > 0" value="Follow">Follow</option>
-                <option v-if="leadSpots > 0" value="Lead">Lead</option>
-                <!-- <option value="Any">Any</option> -->
-            </select>
-
-            <div>What's your zip code?</div>
-            <input type="text" v-model="zip"/>
-
-            <div>And your email address?</div>
-            <input type="text" v-model="email"/>
-
-            <div>Anything else you'd like to share?</div>
-            <textarea rows="4" v-model="comments"></textarea>
-
-            <div v-if="!submitted" style="text-align: center">
-                <button type="submit" :disabled="sending">RSVP</button>
-                <span v-if="sending" style="margin-left: 1ex">Sending ...</span>
+                <p>If you'd like to be added to the waitlist,
+                please <a href="/contact">contact us</a>.</p>
             </div>
 
             <div v-else>
                 <p>
-                Thank you! You should receive an email within an hour to
-                confirm that everything went through. -- P</p>
+                    Please enter some information to reserve your spot in the class 
+                    and help us understand where you're coming from.
+                </p>
+                <label>
+                    <div>Name</div>
+                    <input type="text" v-model="name"/>
+                </label>
+
+                <!-- <div class="agreement">
+                    <div style="margin-bottom: 0.5ex">Please confirm your availability:</div>
+                    <input 
+                        id="agreed" 
+                        type="checkbox"
+                        v-model="agreed"/>
+                    <label for="agreed">I will be at the first session (April 3) and
+                    the second session (April 10).</label>
+                </div> -->
+
+                <div>What dance role would you like to have during this class?</div>
+
+                <p>Swing dancing is a conversation, typically between two people. The person who is responsible for moving first is referred to as a lead, whereas the person who moves second is referred to as a follow. Most people who dance long enough learn both roles.</p>
+                
+                <div>Follow spots remaining: {{followSpots}}</div>
+                <div style="margin-bottom: 1em">Lead spots remaining: {{leadSpots}}</div>
+
+                <p v-if="waitlistOpen">
+                    If you'd like to be added to the waitlist for this course,
+                    please <a href="/contact">contact us</a>.
+                </p>
+
+                <select v-model="role">
+                    <option value="null">-- Please select --</option>
+                    <option v-if="followSpots > 0" value="Follow">Follow</option>
+                    <option v-if="leadSpots > 0" value="Lead">Lead</option>
+                    <!-- <option value="Any">Any</option> -->
+                </select>
+
+                <div>What's your zip code?</div>
+                <input type="text" v-model="zip"/>
+
+                <div>And your email address?</div>
+                <input type="text" v-model="email"/>
+
+                <div>Anything else you'd like to share?</div>
+                <textarea rows="4" v-model="comments"></textarea>
+
+                <div v-if="!submitted" style="text-align: center">
+                    <button type="submit" :disabled="sending">RSVP</button>
+                    <span v-if="sending" style="margin-left: 1ex">Sending ...</span>
+                </div>
+
+                <div v-else>
+                    <p>
+                    Thank you! You should receive an email within an hour to
+                    confirm that everything went through. -- P</p>
+                </div>
+
+                <p v-if="error" v-cloak>
+                    Sorry, there was an error when we tried to submit your form.
+                    Please let us know at rsvp@dance.coop?
+                </p>
+
+                <!-- <p v-if="needAgreement" class="need-agreement">
+                    <strong>Attention:</strong> To RSVP, please mark that you can attend the first class and the second class, above, and try submitting the form again.
+                </p> -->
             </div>
-
-            <p v-if="error" v-cloak>
-                Sorry, there was an error when we tried to submit your form.
-                Please let us know at rsvp@dance.coop?
-            </p>
-
-            <!-- <p v-if="needAgreement" class="need-agreement">
-                <strong>Attention:</strong> To RSVP, please mark that you can attend the first class and the second class, above, and try submitting the form again.
-            </p> -->
         </form>
 
     </section>
@@ -116,6 +125,9 @@ export default {
         },
         waitlistOpen() {
             return (this.followSpots === 0 || this.leadSpots === 0);
+        },
+        courseFull() {
+            return (this.followSpots === 0 && this.leadSpots === 0);
         }
     },
     created() {
